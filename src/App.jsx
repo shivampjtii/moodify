3a6fc73772b7e8bf74b99dcd35e412c11152569e
shivampjtii <shivampjtii@gmail.com>
2026-02-26 13:46:54 +0530

    face detection

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import FaceExpression from './features/Expression/components/FaceExpression'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <FaceExpression/>
+    </>
+  )
+}
+
+export default App
