0b05c666bdd89c78a58fd8dcf6cf2c1884d1af81
shivampjtii <shivampjtii@gmail.com>
2026-03-04 11:29:42 +0530

    context

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router"
 import { router } from "./app.routes"
 import "./features/shared/styles/global.scss"
+import { AuthProvider } from "./features/auth/auth.context"
 
 function App() {
 
   return (
-    <RouterProvider router={router}/>
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
   )
 }
 
