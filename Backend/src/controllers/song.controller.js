5920819b346adca75f7e3616f51744c3d0671bbb
shivampjtii <shivampjtii@gmail.com>
2026-03-08 14:45:05 +0530

    backend

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -45,6 +45,17 @@ async function uploadSong(req,res){
     })
 }
 
+async function getSong(req,res){
+    const { mood } = req.query
+    const song = await songModel.findOne({mood})
+
+    res.status(200).json({
+        message: "song fetched",
+        song
+    })
+}
+
 module.exports = {
-    uploadSong
+    uploadSong,
+    getSong
 }
--- Backend/src/routes/song.routes.js
+++ Backend/src/routes/song.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const upload = require("../middleware/upload.middleware");
-const { uploadSong } = require("../controllers/song.controller");
+const { uploadSong, getSong } = require("../controllers/song.controller");
 const router = express.Router();
 
 router.post("/", upload.single("song"), uploadSong)
+router.get("/", getSong)
 
 module.exports = router;
